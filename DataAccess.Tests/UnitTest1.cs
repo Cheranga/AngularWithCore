2bedc05517256f68e78bfb782fa57333a508f031
cheranga <cheranga@gmail.com>
2018-05-12 09:52:06 +1000

    Adding "develop" branch

---
+++ DataAccess.Tests/UnitTest1.cs
@@ -0,0 +1,44 @@
+using System;
+using AngularWithCore.Models;
+using Xunit;
+
+namespace DataAccess.Tests
+{
+    public class EmployeeDataAccessTests
+    {
+        [Fact]
+        public void GetAllEmployees()
+        {
+            var dataAccess = new EmployeeDataAccessLayer();
+            var employees = dataAccess.GetAll();
+
+        }
+
+        [Fact]
+        public void AddEmployee()
+        {
+            var employee = new Employee
+            {
+                Name = "Cheranga",
+                Department = "IT",
+                City = "Melbourne",
+                Gender = "Male"
+            };
+
+            var dataAccess = new EmployeeDataAccessLayer();
+            var result = dataAccess.AddEmployee(employee);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void UpdateEmployee()
+        {
+            var employee = new Employee
+            {
+
+            }
+        }
+
+    }
+}
--- RefAngularWithCore/CRUD.ASPCore.Angular5.WebAPI.ADO-master/ASPCoreWithAngular/Startup.cs
+++ RefAngularWithCore/CRUD.ASPCore.Angular5.WebAPI.ADO-master/ASPCoreWithAngular/Startup.cs
@@ -2,14 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AngularWithCore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace AngularWithCore
+namespace ASPCoreWithAngular
 {
     public class Startup
     {
@@ -23,8 +22,6 @@ namespace AngularWithCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<EmployeeDataAccessLayer>();
-
             services.AddMvc();
         }
 
